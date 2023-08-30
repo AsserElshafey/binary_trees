32e834f3a03547d5502bde625f49d97efb96bf08
loay <loaywaleed9@gmail.com>
2023-08-30 22:46:42 +0300

    hot fix

--- 7-binary_tree_inorder.c
+++ 7-binary_tree_inorder.c
@@ -1,6 +1,6 @@
 #include "binary_trees.h"
 /**
- * binary_tree_preorder - in-order traversal
+ * binary_tree_inorder - in-order traversal
  * @tree: tree to traverse
  * @func: pointer to function to call for each node
  */
@@ -11,7 +11,7 @@ void binary_tree_inorder(const binary_tree_t *tree, void (*func)(int))
 	{
 		return;
 	}
-	binary_tree_preorder(tree->left, func);
+	binary_tree_inorder(tree->left, func);
 	func(tree->n);
-	binary_tree_preorder(tree->right, func);
+	binary_tree_inorder(tree->right, func);
 }
