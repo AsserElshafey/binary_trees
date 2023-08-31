24c5c5e980e8fe5f2fd5ac9db5c85f5f4725b76a
loay <loaywaleed9@gmail.com>
2023-08-31 10:03:46 +0300

    Hot fix

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -14,8 +14,8 @@ size_t binary_tree_height(const binary_tree_t *tree)
 	{
 		return (0);
 	}
-	left_height = height(node->left);
-	right_height = height(node->right);
+	left_height = height(tree->left);
+	right_height = height(tree->right);
 	if (left_height > right_height)
 	{
 		return (left_height + 1);
