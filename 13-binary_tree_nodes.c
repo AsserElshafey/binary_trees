8afde6f71d6482797d14dc93f901649b57b3b627
loay <loaywaleed9@gmail.com>
2023-08-31 14:02:57 +0300

    Number of nodes in a binary tree

---
+++ 13-binary_tree_nodes.c
@@ -0,0 +1,23 @@
+#include "binary_trees.h"
+/**
+ * binary_tree_nodes - finding number of nodes  of the binary tree
+ * @tree: root of the tree
+ * Return: leaves of binary tree, 0 on null or failure
+ */
+
+size_t binary_tree_nodes(const binary_tree_t *tree)
+{
+	size_t nodes = 0;
+
+	if (tree == NULL)
+	{
+		return (0);
+	}
+	if (((tree->left) == NULL) || ((tree->right) == NULL))
+	{
+		nodes++;
+	}
+	nodes += binary_tree_leaves(tree->left);
+	nodes += binary_tree_leaves(tree->right);
+	return (nodes);
+}
